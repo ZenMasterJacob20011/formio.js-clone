9b0e3adf94a80dd8b33a7107007c33761f1657c5
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-05-25 20:24:38 -0500

    added webpack dev server

---
+++ webpack.dev.mjs
@@ -0,0 +1,23 @@
+import path from 'path'
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = import.meta.dirname
+export default {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: path.resolve(__dirname,'index.html')
+        })
+    ]
+    ,
+    mode: 'development'
+}
