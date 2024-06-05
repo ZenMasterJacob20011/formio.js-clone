3f6c484edb88eed2b32a1b81bd087879c768969b
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-06-04 23:48:09 -0500

    Added password component

---
+++ src/components/Password.js
@@ -0,0 +1,19 @@
+import TextField from './TextField';
+
+export default class Password extends TextField {
+    static schema(...extend) {
+        return TextField.schema({
+            type: 'password',
+            key: 'password',
+            label: 'Password'
+        }, ...extend)
+    }
+
+    constructor(component, data, options) {
+        super(component, data, options);
+    }
+
+    get defaultSchema() {
+        return Password.schema();
+    }
+}
