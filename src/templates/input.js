3320a9573be3489bdeb78d04a0ab669238a51143
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-07-04 19:47:39 -0500

    removed textarea predefined space

--- src/templates/input.js
+++ src/templates/input.js
@@ -5,6 +5,5 @@ export default function (ctx) {
             class="form-control" 
             id="${ctx.component._id}" 
             type="${ctx.component.type}"
-            placeholder="${ctx.component.placeholder}">
-            ${ctx.component.type === 'textarea' ? '</textarea>' : ''}`;
+            placeholder="${ctx.component.placeholder}">${ctx.component.type === 'textarea' ? '</textarea>' : ''}`;
 }
