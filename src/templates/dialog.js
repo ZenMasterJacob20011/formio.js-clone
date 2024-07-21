b0c953092aab3d1bb9bdbcc5452074f9976eaea5
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-07-20 22:25:37 -0500

    removed button border

--- src/templates/dialog.js
+++ src/templates/dialog.js
@@ -7,7 +7,7 @@ export default function(ctx){
     return `
         <div ref="dialog" class="formio-dialog">
             <div class="formio-dialog-contents">
-                <button ref="dialogClose" class="formio-dialog-close float-end btn-sm">x</button>
+                <button ref="dialogClose" class="formio-dialog-close float-end btn btn-lg"></button>
                 ${ctx.dialogContents}
             </div>
         </div>
