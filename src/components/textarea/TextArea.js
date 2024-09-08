31755d83f7241c0a621c270a7156697a38eaec96
ZenMasterJacob20011 <jacobnichols@form.io>
2024-09-07 19:01:18 -0500

    added ace editor

--- src/components/textarea/TextArea.js
+++ src/components/textarea/TextArea.js
@@ -1,19 +1,21 @@
-
 import TextAreaForm from './TextArea.form';
 import Input from '../_classes/input/Input';
+import {requireLibrary} from '../../utils/utils';
 
 export default class TextArea extends Input {
 
     static editInfo = TextAreaForm;
-static schema(...extend) {
+
+    static schema(...extend) {
         return Input.schema({
             label: 'Text Area',
             key: 'textArea',
-            type: 'textarea'
+            type: 'textarea',
+            editor: ''
         }, ...extend);
     }
 
-    static get builderInfo(){
+    static get builderInfo() {
         return {
             title: 'Text Area',
             group: 'basic',
@@ -22,13 +24,52 @@ static schema(...extend) {
         };
     }
 
-    
 
     constructor(component, options, data) {
         super(component, options, data);
+        this.editorReady = {};
+    }
+
+    attach(element) {
+        super.attach(element);
+        if (this.component.editor === 'ace') {
+            this.editorReady.ace = new Promise((resolve, reject) => {
+                requireLibrary('https://www.unpkg.com/ace-builds@1.36.2/src-noconflict/ace.js', 'ace').then((ace) => {
+                    this.editor = ace.edit(this.refs.input, {
+                        mode: 'ace/mode/json',
+                        selectionStyle: 'text'
+                    });
+                    resolve(this.editor);
+                }).catch(reject);
+            });
+        }
     }
 
     get defaultSchema() {
         return TextArea.schema();
+    }
+
+    getValue() {
+        if (this.component.editor === 'ace') {
+            return this.editor.getValue();
+        }
+        return super.getValue();
+    }
+
+    normalizeValue(value) {
+        if (typeof value === 'object') {
+            return JSON.stringify(value, null, 2);
+        }
+    }
+
+
+    setValue(value) {
+        if (this.component.editor === 'ace') {
+            this.editorReady.ace.then((editor) => {
+                editor.setValue(this.normalizeValue(value));
+            });
+        } else {
+            super.setValue(value);
+        }
     }
 }
--- src/templates/input.js
+++ src/templates/input.js
@@ -6,10 +6,19 @@
 export default function (ctx) {
     return `
             <label for="${ctx.component._id}-${ctx.component.type}">${ctx.component.label}</label>
-            <${ctx.component.type === 'textarea' ? 'textarea' : 'input'} 
-            class="form-control"
-            ref="input"
-            id="${ctx.component._id}-${ctx.component.type}" 
-            type="${ctx.component.type}"
-            placeholder="${ctx.component.placeholder}">${ctx.component.type === 'textarea' ? '</textarea>' : ''}`;
+            ${(function(){
+                if(ctx.component.editor === 'ace'){
+                    return `<div ref="input" id="${ctx.component._id}-${ctx.component.type}" type="${ctx.component.type}" style="width: ${ctx.component.width}; height: ${ctx.component.height}"></div>`;
+                }else{
+                    return ` 
+                        <${ctx.component.type === 'textarea' ? 'textarea' : 'input'}
+                          class="form-control"
+                          ref="input"
+                          id="${ctx.component._id}-${ctx.component.type}"
+                          type="${ctx.component.type}"
+                          placeholder="${ctx.component.placeholder}">${ctx.component.type === 'textarea' ? '</textarea>' : ''} 
+                        `;
+                }
+            })()}
+            `;
 }
