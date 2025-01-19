323e26c26faa248432cb6b0781884fdb3de53da9
ZenMasterJacob20011 <jacobnichols@form.io>
2025-01-18 20:18:23 -0600

    fix vertical tabs css

--- src/templates/tabs.js
+++ src/templates/tabs.js
@@ -7,14 +7,14 @@ export default function(ctx){
     return `
         <div class="card" ref="${ctx.componentContext.nestedKey}">
             <div class="row g-0">
-                ${ctx.componentContext.component.verticalLayout ? '<div class="col-1">' : ''}
+                ${ctx.componentContext.component.verticalLayout ? '<div class="col-auto">' : ''}
                     <div class="card-header">
                         <ul class="nav nav-tabs ${ctx.componentContext.component.verticalLayout ? 'flex-column' : ''}" role="tablist">
                             ${(function(){
                                let theHTML = '';
                                ctx.componentContext.component.components.forEach((component, index) => {
                                    theHTML += `<li ref="${component.key}" class="nav-item" role="presentation">
-                                                    <button ref="tab-link" class="nav-link ${index === ctx.currentTab ? 'active' : ''}" data-bs-toggle="tab" data-bs-target="#${component.key}" type="button" role="tab" aria-selected="${!index ? 'true' : 'false'}">${component.label}</button>
+                                                    <button ref="tab-link" class="nav-link w-100 text-start ${index === ctx.currentTab ? 'active' : ''}" data-bs-toggle="tab" data-bs-target="#${component.key}" type="button" role="tab" aria-selected="${!index ? 'true' : 'false'}">${component.label}</button>
                                                 </li>`;
                                });
                                return theHTML;
